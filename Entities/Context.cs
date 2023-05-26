3f85ee0cc149ce42a9f6d5ae640e17654a4eb98a
ChienTQ2003 <anhchienbs.1@gmail.com>
2023-05-26 19:47:58 +0700

    first commit

---
+++ Entities/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Exam.Entities
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<SubjectExam> SubjectExam { get; set; }
+    }
+}
---
+++ Entities/SubjectExam.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Exam.Entities
+{
+    [Table("SubjectExam")]
+    public class SubjectExam
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string SubJectName { get; set; }
+        [Required]
+        public string StartTime { get; set; }
+        [Required]
+        public DateTime ExamDate { get; set; }
+        [Required]
+        public string Faculty { get; set; }
+    }
+}
